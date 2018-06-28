707b56f01bb4232747d3a211fe97536b3d64b895
Luis Tejero <luisfaridta@gmail.com>
2018-06-28 14:47:47 +0100

    helper refactored using fetch

--- src/models/munros.js
+++ src/models/munros.js
@@ -6,14 +6,27 @@ const Munros = function (url) {
   this.munros = [];
 };
 
+// Munros.prototype.getData = function () {
+//   const request = new RequestHelper(this.url);
+//   request.get(data => this.handleData(data));
+// };
+
 Munros.prototype.getData = function () {
   const request = new RequestHelper(this.url);
-  request.get(data => this.handleData(data));
-};
+  const handleRequestComplete = (responseData) => {
+    this.munros = responseData;
+    PubSub.publish('Munros:munros-data-ready', this.munros);
+  };
+  request.get()
+    .then(handleRequestComplete)
+    .catch(error => console.error(error));
+}
 
-Munros.prototype.handleData = function (data) {
-  this.munros = data;
-  PubSub.publish('Munros:munros-data-ready', this.munros);
-};
+// Munros.prototype.handleData = function (data) {
+//   this.munros = data;
+//   PubSub.publish('Munros:munros-data-ready', this.munros);
+// };
+//
+
 
 module.exports = Munros;
